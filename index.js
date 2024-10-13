529a82d0f8e5ade4a802dce0475afeafe013d915
javiergoga <156128804+javiergoga@users.noreply.github.com>
2024-10-13 17:44:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const header = document.querySelector("header");
+  header.style.opacity = "1"; // Cambia la opacidad para hacer visible el header
+  header.style.transform = "scale(1)"; // Aplica el zoom out
+};
