68e4d2cb72d45a7df97c45ef2321b657cd7c0e40
javiergoga <156128804+javiergoga@users.noreply.github.com>
2024-10-13 20:51:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.onload = function () {
   const header = document.querySelector("header");
-  header.style.opacity = "1"; // Cambia la opacidad para hacer visible el header
-  header.style.transform = "scale(1)"; // Aplica el zoom out
+  requestAnimationFrame(() => {
+    header.style.opacity = "1"; // Cambia la opacidad para hacer visible el header
+    header.style.transform = "scale(1)"; // Aplica el zoom out
+  });
 };
