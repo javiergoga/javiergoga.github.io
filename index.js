e56c9a8de0d79a60de0b57fba072782c380fb280
javiergoga <156128804+javiergoga@users.noreply.github.com>
2024-10-19 21:31:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 window.onload = function () {
   const header = document.querySelector("header");
   requestAnimationFrame(() => {
-    header.style.opacity = "1"; // Cambia la opacidad para hacer visible el header
-    header.style.transform = "scale(1)"; // Aplica el zoom out
+    header.style.opacity = "1";
+    header.style.transform = "scale(1)";
   });
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Mostrar el indicador tras 6 segundos
   setTimeout(function () {
-    document.querySelector(".scroll-indicator").style.opacity = "1"; // Aparece el indicador
+    document.querySelector(".scroll-indicator").style.opacity = "1";
   }, 8000);
 
-  // Desaparecer el indicador al hacer scroll
   window.addEventListener("scroll", function () {
-    document.querySelector(".scroll-indicator").style.opacity = "0"; // Desaparece el indicador
+    document.querySelector(".scroll-indicator").style.opacity = "0";
   });
 });
